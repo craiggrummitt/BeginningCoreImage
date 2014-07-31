30f2e91e5df45e1142a4c359cde5d03add831bef
Craig Grummitt <craiggrummitt@gmail.com>
2014-07-31 16:01:43 -0500

    adjust sepia tone to image

--- CoreImageExperiments/ViewController.swift
+++ CoreImageExperiments/ViewController.swift
@@ -10,10 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
                             
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var slider: UISlider!
+    @IBAction func sliderChanged(sender: AnyObject) {
+        filterImage()
+    }
+    var context:CIContext = CIContext(options: nil)
+    var filter:CIFilter = CIFilter(name:"CISepiaTone")
+    var beginImage = CIImage(image:UIImage(named: "flowers"))
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        println(UIImage(named: "flowers"))
+        filter.setValue(beginImage, forKey: kCIInputImageKey)
+        
+        filterImage()
+        
     }
+    
+    func filterImage() {
+        
+        filter.setValue(slider.value, forKey: kCIInputIntensityKey)
+        var outputImage = filter.outputImage;
+        println(filter)
+        //        var newImage = UIImage(CIImage: outputImage)                          //<--removed now using context
+        var cgimg = context.createCGImage(outputImage, fromRect: outputImage.extent()) //<--new line with context
+        
+        var newImage = UIImage(CGImage: cgimg)
+        self.imageView.image = newImage
+    }
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
